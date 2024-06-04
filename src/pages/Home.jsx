2634cd010ddc7c721bdefeaec4535b2cb4c77683
dmharrison <dmharrison2014@gmail.com>
2024-06-03 20:31:43 -0400

    added assets folder with the 3d glb of the shipwreck and got it positioned on the screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,49 @@
 import {Suspense} from 'react'
 import { Canvas} from '@react-three/fiber'
 import Loader from '../components/Loader'
+import Ship from '../models/ship'
+
+
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POPUP</div> */}
 
 
 
 const Home = () => {
+  const adjustShipWreckForScreenSize =()=>{
+    let screenScale=null; 
+    let screenPosition=[100,-125,-220];
+    let rotation =[0.1, 4.7,0]
+
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9];
+      
+    }else{
+      screenScale = [1, 1, 1];
+     
+    }
+    return [screenScale,screenPosition,rotation];
+  }
+  const [shipWreckScale, shipWreckPosition, shipWreckRotation] = adjustShipWreckForScreenSize();
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
         className="w-full h-screen bg-transparent"
         camera={{near:0.1, far:1000}}
         >
-<Suspense fallback={<Loader />}>
+      <Suspense fallback={<Loader />}>
+        <directionalLight positon={[1,1,1]} intensity={2}/>
+        <ambientLight intensity={1}/>
+        <pointLight  />
+        <spotLight />
+        <hemisphereLight />
+        <Ship position={shipWreckPosition}
+        scale={shipWreckScale}
+        rotation={shipWreckRotation}
 
-</Suspense>
+        />
+      </Suspense>
       </Canvas>
 
     </section>
