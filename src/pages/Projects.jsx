4f680d497138b8601cc5c0862d555a8b61aaea44
dmharrison <dmharrison2014@gmail.com>
2024-06-05 15:51:10 -0400

    wrapped the cards with a relative tag and the  put the two btns in their own divs a moved them positioned absolutely to the bottom left and right

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,29 +12,34 @@ const Projects = () => {
 
     <div className='flex flex-wrap my-20 gap-16'>
       {projects.map((project)=>(
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
         <a href="#">
             <img className="rounded-t-lg" src={project.image} alt="" />
         </a>
-        <div className="p-5">
+        <div className="p-5 pb-16">
             <a href="#">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
             </a>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
+        </div>
+        <div className="absolute bottom-4 left-4">
             <a href={project.live} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Live Site
-                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
             </a>
+        </div>
+        <div className="absolute bottom-4 right-4">
             <a href={project.githublink} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Github
-                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
             </a>
         </div>
     </div>
+    
 
 
       ))}
